56c49a02c9c7c96fde19c7f4333b3a56828871ad
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-07-21 16:24:05 +0100

    fix

--- AuthenticationService/src/main/java/com/introtech/authenticationservice/AuthenticationServiceApplication.java
+++ AuthenticationService/src/main/java/com/introtech/authenticationservice/AuthenticationServiceApplication.java
@@ -3,7 +3,7 @@ package com.introtech.authenticationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.introtech")
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
